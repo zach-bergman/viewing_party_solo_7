6bbb4ed4f4381d7ef97359333077ee53f323386f
Zach Bergman <bzach634@gmail.com>
2024-07-04 12:47:02 -0600

    PS5 functionality

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :create] do
     resources :discover, only: :index
     resources :movies, only: [:index, :show] do
-      resources :viewing_party, only: [:new, :create]
+      resources :viewing_party, only: [:new, :create, :show]
     end
   end
 end
